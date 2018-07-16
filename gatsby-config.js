6752baa0adf40b428ef8587d49569aba2fc02d6f
Yomi Toba <ytwguru@gmail.com>
2018-07-16 14:04:34 -0500

    Remove gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+
 let activeEnv = process.env.ACTIVE_ENV;
 let baseUrl = "";
 let protocol = "";
@@ -24,31 +25,6 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-next`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'HEC-TV',
-        short_name: 'HEC-TV',
-        description: 'On Demand Arts, Culture & Education Programming',
-        homepage_url: 'https://hectv.org',
-        start_url: '/',
-        background_color: '#fff',
-        theme_color: '#ffffff',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'src/assets/favicons/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'src/assets/favicons/android-chrome-384x384.png',
-            sizes: '384x384',
-            type: 'image/png',
-          },
-        ],
-      },
-    },
     {
       resolve: `gatsby-source-wordpress`,
       options: {
