a974c9b390dfd6068e5d2765c4dc41640f6a62bd
Yomi Toba <ytwguru@gmail.com>
2018-07-16 17:47:28 -0500

    Update the favicon design

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -44,6 +44,7 @@ export default ({
   <link rel="icon" type="image/png" sizes="32x32" href={withPrefix("/favicons/favicon-32x32.png")} />
   <link rel="icon" type="image/png" sizes="96x96" href={withPrefix("/favicons/favicon-96x96.png")} />
   <link rel="icon" type="image/png" sizes="16x16" href={withPrefix("/favicons/favicon-16x16.png")} />
+  <link rel="shortcut icon" href={withPrefix("/favicons/favicon.ico")} />
   <meta name="msapplication-TileColor" content="#ffffff" />
   <meta name="theme-color" content="#ffffff" />
   <meta name="msapplication-TileImage" content={withPrefix("/favicons/ms-icon-144x144.png")} />
