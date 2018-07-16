d050a6f0c6f10ba59bb47fa4ec7775030245526a
Yomi Toba <ytwguru@gmail.com>
2018-07-16 14:40:34 -0500

    Change Helmet setup

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import midSizedFavicon from "./../../assets/favicons/favicon-32x32.png"
 import smallFavicon from "./../../assets/favicons/favicon-16x16.png"
+import appleFavicon from "./../../assets/favicons/apple-touch-icon.png"
+import safariFavicon from "./../../assets/favicons/safari-pinned-tab.svg"
 
 export default ({
       description,
@@ -12,29 +14,53 @@ export default ({
       pathname,
       twitter_handle,
       url
-    }) => <Helmet>
-  <title>{title}</title>
-  <meta name="description" content={description || ''}  />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta name="og:image" content={image || ''}  />
-  <meta name="og:title" content={title || ''}  />
-  <meta name="og:image:width" content="650"  />
-  <meta name="og:image:height" content="497"  />
-  <meta name="og:site_name" content={site_name || ''}  />
-  <meta name="og:locale'" content="en_US"  />
-  <meta name="og:url" content={`${url}${pathname}`}  />
-  <meta name="og:type" content='article'  />
-
-  <meta name="twitter:image" content={image || ''}  />
-  <meta name="twitter:title" content={title || ''}  />
-  <meta name="twitter:description" content={description || ''}  />
-  <meta name="twitter:card" content="summary_large_image"  />
-  <meta name="twitter:creator" content={twitter_handle || ''}  />
-
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-  <link rel="icon" type="image/png" sizes="32x32" href={midSizedFavicon} />
-  <link rel="icon" type="image/png" sizes="16x16" href={smallFavicon} />
-  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-  <meta name="msapplication-TileColor" content="#da532c" />
-  <meta name="theme-color" content="#ffffff" />
-</Helmet>
+                }) => <Helmet
+  meta={[
+    {
+      name: 'description',
+      content: description || ''
+    },
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0'
+    },
+    {
+      property: 'og:image',
+      content: image || ''
+    },
+    { property: 'og:title', content: title || '' },
+    {
+      property: 'og:description',
+      content: description || ''
+    },
+    { property: 'og:image:width', content: '650' },
+    { property: 'og:image:height', content: '497' },
+    { property: 'og:site_name', content: site_name || '' },
+    { property: 'og:locale', content: 'en_US' },
+    { property: 'og:url', content: `${url}${pathname}` },
+    { property: 'og:locale', content: 'en_US' },
+    { property: 'og:type', content: 'article' },
+    { name: 'author', content: author || '' },
+    { name: 'twitter:title', content: title || '' },
+    {
+      name: 'twitter:description',
+      content: description || ''
+    },
+    { name: 'twitter:site', content: site_name },
+    {
+      name: 'twitter:image',
+      content: image || ''
+    },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    { name: 'twitter:creator', content: twitter_handle },
+    { name : 'msapplication-TileColor', content: "#da532c"},
+    { name : 'theme-color', content: "#ffffff"}
+  ]}
+  link={[
+    { rel: 'icon', type:"image/png", sizes : "16x16", href : smallFavicon },
+    { rel: 'icon', type:"image/png", sizes : "32x32", href : midSizedFavicon },
+    { rel: 'apple-touch-icon', type:"image/png", sizes : "180x180", href : appleFavicon },
+    { rel: 'mask-icon', color : "#5bbad5", href : safariFavicon },
+  ]}
+  title={title}
+>
