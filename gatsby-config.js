a99118d6c96a106712be83363902e4edcbb378bf
Yomi Toba <ytwguru@gmail.com>
2018-07-16 13:24:38 -0500

    Add manifest file and lazyloading

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,34 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-next`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'HEC-TV',
+        short_name: 'HEC-TV',
+        description: 'On Demand Arts, Culture & Education Programming',
+        homepage_url: 'https://hectv.org',
+        start_url: '/',
+        background_color: '#fff',
+        theme_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'src/assets/favicons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'src/assets/favicons/android-chrome-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+        ],
+      },
+    },
     {
       resolve: `gatsby-source-wordpress`,
       options: {
