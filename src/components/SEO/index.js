d346639ae1a12fe83f950f6904798db0e9572671
Yomi Toba <ytwguru@gmail.com>
2018-07-16 16:50:25 -0500

    Move favicons to pages directory

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -1,23 +1,5 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import midSizedFavicon from "./../../assets/favicons/favicon-32x32.png";
-import smallFavicon from "./../../assets/favicons/favicon-16x16.png";
-import safariFavicon from "./../../assets/favicons/safari-pinned-tab.svg";
-import appleFavicon_57 from "./../../assets/favicons/apple-icon-57x57.png";
-import appleFavicon_60 from "./../../assets/favicons/apple-icon-60x60.png";
-import appleFavicon_72 from "./../../assets/favicons/apple-icon-72x72.png";
-import appleFavicon_76 from "./../../assets/favicons/apple-icon-76x76.png";
-import appleFavicon_114 from "./../../assets/favicons/apple-icon-114x114.png";
-import appleFavicon_120 from "./../../assets/favicons/apple-icon-120x120.png";
-import appleFavicon_144 from "./../../assets/favicons/apple-icon-144x144.png";
-import appleFavicon_152 from "./../../assets/favicons/apple-icon-152x152.png";
-import appleFavicon_180 from "./../../assets/favicons/apple-icon-180x180.png";
-
-import androidFavicon_190 from "./../../assets/favicons/android-icon-192x192.png";
-import favicon_32 from "./../../assets/favicons/favicon-32x32.png";
-import favicon_96 from "./../../assets/favicons/favicon-96x96.png";
-import favicon_16 from "./../../assets/favicons/favicon-16x16.png";
-import msFavicon_144 from "./../../assets/favicons/ms-icon-144x144.png";
 
 export default ({
       description,
@@ -47,20 +29,20 @@ export default ({
   <meta name="twitter:card" content="summary_large_image"  />
   <meta name="twitter:creator" content={twitter_handle || ''}  />
 
-  <link rel="apple-touch-icon" sizes="57x57" href={appleFavicon_57} />
-  <link rel="apple-touch-icon" sizes="60x60" href={appleFavicon_60} />
-  <link rel="apple-touch-icon" sizes="72x72" href={appleFavicon_72} />
-  <link rel="apple-touch-icon" sizes="76x76" href={appleFavicon_76} />
-  <link rel="apple-touch-icon" sizes="114x114" href={appleFavicon_114} />
-  <link rel="apple-touch-icon" sizes="120x120" href={appleFavicon_120} />
-  <link rel="apple-touch-icon" sizes="144x144" href={appleFavicon_144} />
-  <link rel="apple-touch-icon" sizes="152x152" href={appleFavicon_152} />
-  <link rel="apple-touch-icon" sizes="180x180" href={appleFavicon_180} />
-  <link rel="icon" type="image/png" sizes="192x192"  href={androidFavicon_190} />
-  <link rel="icon" type="image/png" sizes="32x32" href={favicon_32} />
-  <link rel="icon" type="image/png" sizes="96x96" href={favicon_96} />
-  <link rel="icon" type="image/png" sizes="16x16" href={favicon_16} />
+  <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png"/>
+  <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-icon-60x60.png"/>
+  <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-icon-76x76.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-icon-120x120.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-icon-144x144.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-icon-152x152.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png" />
+  <link rel="icon" type="image/png" sizes="192x192"  href="/favicons/android-icon-192x192.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
   <meta name="msapplication-TileColor" content="#ffffff" />
   <meta name="theme-color" content="#ffffff" />
-  <meta name="msapplication-TileImage" content={msFavicon_144} />
+  <meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png" />
 </Helmet>
