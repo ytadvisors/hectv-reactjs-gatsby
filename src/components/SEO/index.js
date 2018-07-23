0b6525207c880f9ae2730a9c9bac77929ecc953e
Yomi Toba <ytwguru@gmail.com>
2018-07-23 09:42:58 -0500

    Rename style.scss to module.scss

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -14,6 +14,7 @@ export default ({
                   url
                 }) => <Helmet>
   <title>{title}</title>
+
   <meta name="description" content={description || ''}  />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta property="og:image" content={image || ''}  />
