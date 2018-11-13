de0d69bf8f84d55a144b3e93caa109daeb289ac0
Yomi Toba <ytwguru@gmail.com>
2018-11-12 21:29:32 -0600

    Fix validator code

--- src/components/ReactForm/Validator.js
+++ src/components/ReactForm/Validator.js
@@ -1,50 +1,45 @@
 import validator from 'validator';
 
 export default fields => {
-  const validateHelper = (values, acc, item) => {
-    let result = { ...acc };
-    const { validation, name } = item;
+  function validateHelper(values, acc, item) {
+    const result = acc;
+    const key = item.name;
+    const validation = item.validation.map(validCheck => {
+      switch (validCheck) {
+        case 'required':
+          if (!values[key]) return 'Required';
+          break;
+        case 'not_empty_array':
+          if (Array.isArray(values[key]) && values[key].length === 0)
+            return 'Required';
+          break;
+        case 'date':
+          if (values[key] === 'MM/DD/YYYY') return 'Invalid date';
+          break;
+        case 'number':
+          if (!validator.isNumeric(`${values[key]}`)) return 'Invalid number';
+          break;
+        case 'email':
+          if (!validator.isEmail(`${values[key]}`))
+            return 'Invalid email address';
+          break;
+        case 'match_password':
+          if (values.password !== values.confirm_password)
+            return 'Passwords do not match';
+          break;
+        default:
+          break;
+      }
+      return null;
+    });
 
-    const response = validation
-      .map(validCheck => {
-        switch (validCheck) {
-          case 'required':
-            if (!values[name]) return 'Required';
-            break;
-          case 'not_empty_array':
-            if (Array.isArray(values[name]) && values[name].length === 0)
-              return 'Required';
-            break;
-          case 'date':
-            if (values[name] === 'MM/DD/YYYY') return 'Invalid date';
-            break;
-          case 'number':
-            if (!validator.isNumeric(`${values[name]}`))
-              return 'Invalid number';
-            break;
-          case 'email':
-            if (!validator.isEmail(`${values[name]}`))
-              return 'Invalid email address';
-            break;
-          case 'matchPassword':
-            if (values.password !== values.confirmPassword)
-              return 'Passwords do not match';
-            break;
-          default:
-            break;
-        }
-        return null;
-      })
-      .filter(n => n);
-
-    if (response && response.length > 0)
-      result = { ...result, name: response[0] };
+    result[key] = validation ? validation.filter(n => n)[0] : null;
     return result;
-  };
+  }
 
   return values =>
     fields.reduce((acc, item) => {
-      let result = { ...acc };
+      let result = acc;
       if (Array.isArray(item)) {
         result = item.reduce(
           (res, it) => validateHelper(values, res, it),
