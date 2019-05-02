84db606d26f4fcdbddafc505ff59e295c1a0abdc
Yomi Toba <ytwguru@gmail.com>
2019-05-02 18:11:36 -0500

    Update to new gatsby version

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,7 @@
+import '@babel/polyfill';
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import wrapWithProvider from './src/utils/wrapWithProvider';
 
-const timeout = 250;
-const historyExitingEventType = `history::exiting`;
-
-export { historyExitingEventType, timeout };
 export const wrapRootElement = wrapWithProvider;
+export default wrapRootElement;
